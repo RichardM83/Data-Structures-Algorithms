001cf63b0fed5b6c5106bb139919580a4f842bb1
Richard Martinez <rjmartinez422@gmail.com>
2023-10-16 22:33:42 -0400

    hello

--- LC1-TwoSum.py
+++
@@ -1,9 +0,0 @@
-def twoSum(self, nums, target):
-    dictionary = {}
-    for i,num in enumerate(nums):
-        complement = target - num
-        if complement in dictionary:
-            return [dictionary[complement], i]
-        else:
-            dictionary[num] = i
-    return []
